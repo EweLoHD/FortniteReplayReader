3a524ea423568ad60f976273b2a9a5ffe8b18271
PizzaCrust <pizzacrustcoder@gmail.com>
2019-06-14 21:49:32 +0700

    delete everything

--- src/fortnitereplayreader/type/EventType.java
+++
@@ -1,9 +0,0 @@
-package fortnitereplayreader.type;
-
-public class EventType {
-
-    public static final String PLAYER_ELIMINATION = "playerElim";
-    public static final String MATCH_STATS = "AthenaMatchStats";
-    public static final String TEAM_STATS = "AthenaMatchTeamStats";
-
-}
